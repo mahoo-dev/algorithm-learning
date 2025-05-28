35282b553eea54c6d4c403da3582d3670d968c5a
Mahoo12138 <Mahoo12138@qq.com>
2025-05-29 07:28:57 +0800

    test(leetcode-73): 补充矩阵置零的测试用例

--- src/73-set-matrix-zeroes.spec.ts
+++ src/73-set-matrix-zeroes.spec.ts
@@ -29,5 +29,11 @@ describe('73-set-matrix-zeroes', () => {
             markingInPlace(nums)
             expect(nums).to.deep.equal([[0,0,0,0],[0,4,5,0],[0,3,1,0]])
         });
+
+        it('[[0,1,2,1],[3,4,0,2],[1,3,1,5]]', () => {
+            const nums = [[0,1,2,1],[3,4,0,2],[1,3,1,5]]
+            markingInPlace(nums)
+            expect(nums).to.deep.equal([[0,0,0,0],[0,0,0,0],[0,3,0,5]])
+        });
     })
 })
